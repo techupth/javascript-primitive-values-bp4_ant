cd7162d89ee3bdccdf958b2cfc1688fb2a6b5cc2
konthron gongmanee <konthron.ant@gmail.com>
2024-06-13 23:07:11 +0700

    feat primitive assignment finish

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discount = (totalPrice) * 25/100
+let Net = (totalPrice) - (discount)
+console.log(Net)
+
+
